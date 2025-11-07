6c5e114ef2657dd1b39b6117df680f65907d1b56
kliewUS <kyuliew@gmail.com>
2025-11-07 10:16:42 -0800

    Team A - Added Metal support for inference code.

--- teams/team-A/predict.py
+++ teams/team-A/predict.py
@@ -73,7 +73,7 @@ def main():
     args = parser.parse_args()
 
     image_paths = ensure_list(list(glob(os.path.join(args.imagedir, "*.*"))))
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
     # Build a model instance and load weights
     # Note: ConvolutionalAutoencoder __init__ expects loaders; pass None (not used in predict)
